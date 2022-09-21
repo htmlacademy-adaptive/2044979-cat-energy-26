1420037e6089b4c63397f6310ab9f0fa5aa9cba4
Keks <keks@htmlacademy.ru>
2022-09-21 22:11:59 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/script.js
+++ 9/js/script.js
@@ -1,14 +1,1 @@
-let navMain = document.querySelector('.main-header');
-let navToggle = document.querySelector('.main-header__toggle');
-
-navMain.classList.remove('main-header__page--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header__page--closed')) {
-    navMain.classList.remove('main-header__page--closed');
-    navMain.classList.add('main-header__page--open');
-  } else {
-    navMain.classList.add('main-header__page--closed');
-    navMain.classList.remove('main-header__page--open');
-  }
-});
+let navMain=document.querySelector(".main-header"),navToggle=document.querySelector(".main-header__toggle");navMain.classList.remove("main-header__page--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("main-header__page--closed")?(navMain.classList.remove("main-header__page--closed"),navMain.classList.add("main-header__page--open")):(navMain.classList.add("main-header__page--closed"),navMain.classList.remove("main-header__page--open"))}));
